eb9173433d71c36b6e8afc14e3d08313ee0d7282
NikitaTaverz <nikita3kovalev3@gmail.com>
2024-04-27 13:08:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# test_savage
+ Test navigation system
---
+++ lib/navigation_layer_test/layer_1_1.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/diagnostics.dart';
+
+class Layer_1_1 extends StatefulWidget {
+  const Layer_1_1({super.key});
+
+  @override
+  State<Layer_1_1> createState() {
+    print('Layer_1_1 -> Widget - createState');
+    return _Layer_1_1State();
+  }
+
+  @override
+  StatefulElement createElement() {
+    print('Layer_1_1 -> Widget - createElement');
+    return super.createElement();
+  }
+}
+
+class _Layer_1_1State extends State<Layer_1_1> {
+  @override
+  Widget build(BuildContext context) {
+    print('Layer_1_1 -> build');
+    return const Placeholder();
+  }
+
+  @override
+  void initState() {
+    print('Layer_1_1 -> initState');
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant Layer_1_1 oldWidget) {
+    print('Layer_1_1 -> didUpdateWidget');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('Layer_1_1 -> didChangeDependencies');
+    super.didChangeDependencies();
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    print('Layer_1_1 -> debugFillProperties');
+    super.debugFillProperties(properties);
+  }
+
+  @override
+  void reassemble() {
+    print('Layer_1_1 -> reassemble');
+    super.reassemble();
+  }
+
+  @override
+  void deactivate() {
+    print('Layer_1_1 -> deactivate');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('Layer_1_1 -> dispose');
+    super.dispose();
+  }
+
+  @override
+  void activate() {
+    print('Layer_1_1 -> activate');
+    super.activate();
+  }
+}
